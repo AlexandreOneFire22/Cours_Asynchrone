082237e3792bdc4c9fc0f1b2170271199d0b3546
GAUTHIER Alexandre <alexgau70@gmail.com>
2025-01-06 11:02:41 +0100

    cool

--- promesse/app.js
+++ promesse/app.js
@@ -1,3 +1,4 @@
+/*
 console.log(1)
 console.log(2)
 
@@ -8,3 +9,96 @@ setTimeout(()=>{
 },3000)
 console.log(3)
 console.log(4)
+ */
+
+let obtenirBTS = new Promise((resolve,reject)=>{
+    // Passer le BTS et obtenir le resultat
+    // Simuler le fait que le resultat est arrivé et positif
+
+    //console.log("cc moi c'est 1")
+
+    setTimeout(()=> {
+
+        let btsObtenu = true
+
+        if (btsObtenu) {
+            // Promesse tenue
+            resolve("BTS obtenu ^^")
+        }else {
+            // Promesse non tenue
+            reject("BTS non obtenue")
+        }
+
+    },3000)
+})
+
+/*
+
+obtenirBTS
+    .then(resultat => {
+    console.log(resultat)
+})                                  //genre de if else
+    .catch(erreur => {
+        console.log(erreur)
+    })
+
+console.log("et moi c'est 2")
+
+*/
+
+//fonction qui retourne une promesse
+//fonction asynchrone
+
+const reussirBTS = () =>{
+    console.log("cc moi c'est 1")
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=> {
+
+            let btsObtenu = true
+
+            if (btsObtenu) {
+                // Promesse tenue
+                resolve("BTS obtenu ^^")
+            }else {
+                // Promesse non tenue
+                reject("BTS non obtenue")
+            }
+
+        },3000)
+    })
+}
+
+//appeler la fonction asynchrone
+
+reussirBTS()
+    .then(resultat => {
+        console.log(resultat)
+    })
+    .catch(erreur => {
+        console.log(erreur)
+    })
+
+console.log("et moi c'est 2")
+
+
+//Autre manière d'appeler la fonction
+//async (asynchrone) et await (attendre la fin de la fonction)
+
+const traiterReussirBTS = async ()=> {
+    try {
+        const reponse = await reussirBTS()
+        console.log(reponse)
+    }catch (error){
+        console.log(error) //Promesse non tenue
+    }
+}
+
+
+
+
+
+
+
+
+
+
